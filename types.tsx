548fbdba8aabeda99a8ccf50ba6ad0e3b774888d
oscarmanas <oscarmanash@gmail.com>
2021-09-03 02:07:14 +0200

    setup bottom tab navigator

--- types.tsx
+++ types.tsx
@@ -1,20 +1,17 @@
-/**
- * Learn more about using TypeScript with React Navigation:
- * https://reactnavigation.org/docs/typescript/
- */
-
 export type RootStackParamList = {
   Root: undefined;
   NotFound: undefined;
 };
 
 export type BottomTabParamList = {
-  TabOne: undefined;
-  TabTwo: undefined;
+  Home: undefined;
+  Coming_Soon: undefined;
+  Search: undefined;
+  Downloads: undefined;
 };
 
-export type TabOneParamList = {
-  TabOneScreen: undefined;
+export type HomeParamList = {
+  HomeScreen: undefined;
 };
 
 export type TabTwoParamList = {
